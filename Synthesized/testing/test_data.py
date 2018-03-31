eb021895ba8997ddf83afeeefefa212ae150ee28
dborovikov <denis.borovikov@soundcloud.com>
2018-03-31 18:28:50 +0200

    Fix data generation so it works with any dimensions

--- Synthesized/testing/test_data.py
+++ Synthesized/testing/test_data.py
@@ -1,5 +1,4 @@
 import numpy as np
-from numpy import linalg as LA
 
 
 def generate_uniform(shape):
@@ -12,20 +11,7 @@ def add_norm_noise(a, std):
 
 
 def add_relative_noise(a, std):
-    norms = LA.norm(x=a, ord=None, axis=1)
+    norms = np.abs(a)
     noise = np.random.normal(loc=0.0, scale=std, size=a.shape)
-    return a + (noise.T * norms).T
+    return a + np.multiply(noise, norms)
 
-
-import matplotlib
-matplotlib.use('TkAgg')
-
-import matplotlib.pyplot as plt
-
-if __name__ == '__main__':
-    data = generate_uniform((20, 2))*100
-    print(data)
-    data_with_noise = add_relative_noise(data, 0.01)
-    plt.scatter(data[:, 0], data[:, 1])
-    plt.scatter(data_with_noise[:, 0], data_with_noise[:, 1])
-    plt.show()
