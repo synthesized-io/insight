74b3b7b407ff7b1b5e8bd87c11e31ca66df5300b
dborovikov <denis.borovikov@soundcloud.com>
2018-04-23 15:23:55 +0200

    Add version module attribute

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '1.0.0'
+exec(open('synthesized/version.py').read())
 
 here = path.abspath(path.dirname(__file__))
 
