e0ace637dc08c332623de42a69d4adecf77f9374
dborovikov <denis.borovikov@soundcloud.com>
2018-05-09 23:59:15 +0200

    Rename transformer to vectorizer

--- tests/test_transaction_demo.py
+++ tests/test_transaction_demo.py
@@ -1,7 +1,8 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 from pandas.testing import assert_frame_equal, assert_series_equal
-from examples.transaction_demo import TransactionTransformer
+
+from examples.transaction_demo import TransactionVectorizer
 
 
 def test_TransactionTransformer():
@@ -9,19 +10,19 @@ def test_TransactionTransformer():
         {'account_id': 'aaa', 'day': 1, 'operation': 1, 'amount': 100.0},
         {'account_id': 'bbb', 'day': 2, 'operation': 2, 'amount': 200.0},
     ])
-    trans = TransactionTransformer(group_column='account_id',
-                                   value_column='amount',
-                                   dim1_column='operation',
-                                   dim2_column='day',
-                                   dim1_size=5,
-                                   dim2_size=32)
+    trans = TransactionVectorizer(group_column='account_id',
+                                  value_column='amount',
+                                  dim1_column='operation',
+                                  dim2_column='day',
+                                  dim1_size=5,
+                                  dim2_size=32)
     df_transformed = trans.fit_transform(df)
 
-    a1 = np.zeros(5*32, dtype=np.float64)
-    a1[1*5 + 1] = 100.0
+    a1 = np.zeros(5 * 32, dtype=np.float64)
+    a1[1 * 5 + 1] = 100.0
 
-    a2 = np.zeros(5*32, dtype=np.float64)
-    a2[2*5 + 2] = 200.0
+    a2 = np.zeros(5 * 32, dtype=np.float64)
+    a2[2 * 5 + 2] = 200.0
 
     df_expected = pd.DataFrame.from_records([a1, a2])
 
