a62e2f960d3222652ce7eb74ebf8dfef3492078a
dborovikov <denis.borovikov@soundcloud.com>
2018-07-23 16:41:04 +0200

    Make test run (but they fail)

--- tests/core/test_basic_synthesizer.py
+++ tests/core/test_basic_synthesizer.py
@@ -1,4 +1,3 @@
-import unittest
 import warnings
 
 import pandas as pd
@@ -10,47 +9,44 @@ from synthesized.testing.testing_environment import Testing
 warnings.filterwarnings(action='ignore', message="numpy.dtype size changed")
 
 
-class TestEncodings(unittest.TestCase):
+def test_basic():
+    data = pd.read_csv('data/transactions.csv')
+    data = data.head(1000)
 
-    def test_basic(self):
-        data = pd.read_csv('data/transactions.csv')
-        # data = data.head(1000)
+    data = data[['type', 'operation', 'amount']]
+    data = data.dropna()
+    data = data[data['type'] != 'VYBER']
+    data['type'] = data['type'].astype(dtype='int')
+    data['type'] = data['type'].astype(dtype='category')
+    data['operation'] = data['operation'].astype(dtype='int')
+    data['operation'] = data['operation'].astype(dtype='category')
+    data['amount'] = data['amount'].astype(dtype='float32')
 
-        data = data[['type', 'operation', 'amount']]
-        data = data.dropna()
-        data = data[data['type'] != 'VYBER']
-        data['type'] = data['type'].astype(dtype='int')
-        data['type'] = data['type'].astype(dtype='category')
-        data['operation'] = data['operation'].astype(dtype='int')
-        data['operation'] = data['operation'].astype(dtype='category')
-        data['amount'] = data['amount'].astype(dtype='float32')
+    with BasicSynthesizer(dtypes=data.dtypes, iterations=1000) as synthesizer:
+        synthesizer.learn(data=data, verbose=5000)
+        synthesized = synthesizer.synthesize(n=10000)
 
-        with BasicSynthesizer(dtypes=data.dtypes) as synthesizer:
-            synthesizer.learn(data=data, verbose=5000)
-            synthesized = synthesizer.synthesize(n=10000)
+    print(data['type'].value_counts(normalize=True, sort=False))
+    print(synthesized['type'].value_counts(normalize=True, sort=False))
+    print(data['operation'].value_counts(normalize=True, sort=False))
+    print(synthesized['operation'].value_counts(normalize=True, sort=False))
+    bins = [
+        min(data['amount'].min(), 0e3), 1e3, 2e3, 3e3, 4e3, 5e3, 6e3, 7e3, 8e3, 9e3,
+        max(data['amount'].max(), 10e3)
+    ]
+    print(pd.cut(data['amount'], bins=bins, include_lowest=True).value_counts(
+        normalize=True, sort=False
+    ))
+    bins = [
+        min(synthesized['amount'].min(), 0e3), 1e3, 2e3, 3e3, 4e3, 5e3, 6e3, 7e3, 8e3, 9e3,
+        max(synthesized['amount'].max(), 10e3)
+    ]
+    print(pd.cut(synthesized['amount'], bins=bins, include_lowest=True).value_counts(
+        normalize=True, sort=False
+    ))
 
-        print(data['type'].value_counts(normalize=True, sort=False))
-        print(synthesized['type'].value_counts(normalize=True, sort=False))
-        print(data['operation'].value_counts(normalize=True, sort=False))
-        print(synthesized['operation'].value_counts(normalize=True, sort=False))
-        bins = [
-            min(data['amount'].min(), 0e3), 1e3, 2e3, 3e3, 4e3, 5e3, 6e3, 7e3, 8e3, 9e3,
-            max(data['amount'].max(), 10e3)
-        ]
-        print(pd.cut(data['amount'], bins=bins, include_lowest=True).value_counts(
-            normalize=True, sort=False
-        ))
-        bins = [
-            min(synthesized['amount'].min(), 0e3), 1e3, 2e3, 3e3, 4e3, 5e3, 6e3, 7e3, 8e3, 9e3,
-            max(synthesized['amount'].max(), 10e3)
-        ]
-        print(pd.cut(synthesized['amount'], bins=bins, include_lowest=True).value_counts(
-            normalize=True, sort=False
-        ))
-
-        testing = Testing(data, data, synthesized)
-        print(testing.estimate_utility(
-            df_orig=data, df_synth=synthesized,
-            continuous_columns=['amount'], categorical_columns=['type', 'operation'],
-            classifier=DecisionTreeClassifier(), regressor=DecisionTreeRegressor()
-        ))
+    testing = Testing(data, data, synthesized)
+    print(testing.estimate_utility(
+        continuous_columns=['amount'], categorical_columns=['type', 'operation'],
+        classifier=DecisionTreeClassifier(), regressor=DecisionTreeRegressor()
+    ))
--- tests/core/test_transformations.py
+++ tests/core/test_transformations.py
@@ -1,32 +1,32 @@
-import unittest
 import numpy as np
 import tensorflow as tf
+
 from synthesized.core.transformations import DenseTransformation, MlpTransformation
 
 
-class TestTransformations(unittest.TestCase):
-
-    def _test_transformation(self, transformation):
-        tf.reset_default_graph()
-        transformation.initialize()
-        transform_input = tf.placeholder(dtype=tf.float32, shape=(None, 8))
-        transform_output = transformation.transform(x=transform_input)
-        initialize = tf.global_variables_initializer()
-        with tf.Session() as session:
-            session.run(fetches=initialize)
-            transformed = session.run(
-                fetches=transform_output, feed_dict={transform_input: np.random.randn(4, 8)}
-            )
-            assert transformed.shape == (4, 6)
-
-    def test_dense(self):
-        transformation = DenseTransformation(
-            name='dense', input_size=8, output_size=6, batchnorm=True, activation='relu'
+def _test_transformation(transformation):
+    tf.reset_default_graph()
+    transformation.initialize()
+    transform_input = tf.placeholder(dtype=tf.float32, shape=(None, 8))
+    transform_output = transformation.transform(x=transform_input)
+    initialize = tf.global_variables_initializer()
+    with tf.Session() as session:
+        session.run(fetches=initialize)
+        transformed = session.run(
+            fetches=transform_output, feed_dict={transform_input: np.random.randn(4, 8)}
         )
-        self._test_transformation(transformation=transformation)
+        assert transformed.shape == (4, 6)
 
-    def test_mlp(self):
-        transformation = MlpTransformation(
-            name='mlp', input_size=8, output_size=6, layer_sizes=(10, 4)
-        )
-        self._test_transformation(transformation=transformation)
+
+def test_dense():
+    transformation = DenseTransformation(
+        name='dense', input_size=8, output_size=6, batchnorm=True, activation='relu'
+    )
+    _test_transformation(transformation=transformation)
+
+
+def test_mlp():
+    transformation = MlpTransformation(
+        name='mlp', input_size=8, layer_sizes=(10, 4)
+    )
+    _test_transformation(transformation=transformation)
