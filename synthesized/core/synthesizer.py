98b03d2f5a6a73cbde7df51c367f40641aafdd7f
dborovikov <denis.borovikov@soundcloud.com>
2018-08-31 11:27:07 +0200

    Add license key file support

--- synthesized/core/synthesizer.py
+++ synthesized/core/synthesizer.py
@@ -7,22 +7,33 @@ from sklearn.base import TransformerMixin
 
 from .module import Module
 
-print("Copyright (C) Synthesized Ltd. - All Rights Reserved")
-license_key = os.environ.get('SYNTHESIZED_KEY', None)
-if license_key is None:
-    raise Exception('No license key detected (check env variable SYNTHESIZED_KEY)')
-else:
-    print('License key: ' + license_key)
-license_key_bytes = base64.b16decode(license_key.replace('-', ''))
-key = 13
-n = 247
-plain = ''.join([chr((char ** key) % n) for char in list(license_key_bytes)])
-date = datetime.strptime(plain.split(' ')[1], "%Y-%m-%d")
-now = datetime.now()
-if now >= date:
-    raise Exception('License has been expired')
-else:
-    print('Expires at: ' + str(date))
+
+def _check_license():
+    key_env = 'SYNTHESIZED_KEY'
+    key_path = '~/.synthesized/key'
+    key_path = os.path.expanduser(key_path)
+    print("Copyright (C) Synthesized Ltd. - All Rights Reserved")
+    license_key = os.environ.get(key_env, None)
+    if license_key is None and os.path.isfile(key_path):
+        with open(key_path, 'r') as f:
+            license_key = f.readlines()[0].rstrip()
+    if license_key is None:
+        raise Exception('No license key detected (env variable {} or {})'.format(key_env, key_path))
+    else:
+        print('License key: ' + license_key)
+    license_key_bytes = base64.b16decode(license_key.replace('-', ''))
+    key = 13
+    n = 247
+    plain = ''.join([chr((char ** key) % n) for char in list(license_key_bytes)])
+    date = datetime.strptime(plain.split(' ')[1], "%Y-%m-%d")
+    now = datetime.now()
+    if now >= date:
+        raise Exception('License has been expired')
+    else:
+        print('Expires at: ' + str(date))
+
+
+_check_license()
 
 
 class Synthesizer(Module, TransformerMixin):
