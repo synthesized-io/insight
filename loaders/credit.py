26cabe8d2655433f6d3c3b20f4366ef80498efa6
Denis Borovikov <denis.borovikov@gmail.com>
2018-10-28 21:40:02 +0000

    Use data library, clean old stuff

--- loaders/credit.py
+++ loaders/credit.py
@@ -1,13 +1,12 @@
 import pandas as pd
 from pathlib import Path
-from sklearn.model_selection import train_test_split
 
 path = Path(__file__).parent.parent / 'data/credit.csv'
 
 
-def load_data(test_size=0.2):
+def load_data():
     data = pd.read_csv(path)
     data.drop('Unnamed: 0', axis=1, inplace=True)
     data.dropna(inplace=True)
     data['SeriousDlqin2yrs'] = data['SeriousDlqin2yrs'].astype(dtype='category')
-    return train_test_split(data, test_size=test_size, random_state=0)
+    return data
