5093cd1648a18d14d4b38585d09cf6b41a1d5e4a
Denis Borovikov <denis.borovikov@gmail.com>
2018-11-15 20:24:14 +0000

    Bump number of iterations

--- tests/core/test_basic_synthesizer.py
+++ tests/core/test_basic_synthesizer.py
@@ -12,7 +12,7 @@ def test_continuous_variable_generation():
     r = np.random.normal(loc=5000, scale=1000, size=1000)
     data = pd.DataFrame({'r': r})
     with BasicSynthesizer(data=data, exclude_encoding_loss=True) as synthesizer:
-        synthesizer.learn(num_iterations=5000, data=data)
+        synthesizer.learn(num_iterations=10000, data=data)
         synthesized = synthesizer.synthesize(n=len(data))
     distribution_distance = ks_2samp(data['r'], synthesized['r'])[0]
     assert distribution_distance < 0.2
@@ -23,7 +23,7 @@ def test_categorical_similarity_variable_generation():
     r = np.random.normal(loc=10, scale=2, size=1000)
     data = pd.DataFrame({'r': list(map(int, r))})
     with BasicSynthesizer(data=data, exclude_encoding_loss=True) as synthesizer:
-        synthesizer.learn(num_iterations=5000, data=data)
+        synthesizer.learn(num_iterations=10000, data=data)
         synthesized = synthesizer.synthesize(n=len(data))
     distribution_distance = ks_2samp(data['r'], synthesized['r'])[0]
     assert distribution_distance < 0.2
@@ -34,7 +34,7 @@ def test_categorical_variable_generation():
     r = np.random.normal(loc=5, scale=1, size=1000)
     data = pd.DataFrame({'r': list(map(int, r))})
     with BasicSynthesizer(data=data, exclude_encoding_loss=True) as synthesizer:
-        synthesizer.learn(num_iterations=5000, data=data)
+        synthesizer.learn(num_iterations=10000, data=data)
         synthesized = synthesizer.synthesize(n=len(data))
     distribution_distance = ks_2samp(data['r'], synthesized['r'])[0]
     assert distribution_distance < 0.2
