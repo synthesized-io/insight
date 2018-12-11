b0bbf730c7098671c1c32ffd10327e53fa72e88d
AlexKuhnle <alexkuhnle@t-online.de>
2018-12-11 16:02:36 +0100

    adaptive iterations for tuning

--- tuning.py
+++ tuning.py
@@ -18,7 +18,6 @@ NUM_RUNS = 3
 print('Parse arguments...')
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dataset', type=str, help="dataset name")
-parser.add_argument('-i', '--iterations', type=int, help="training iterations")
 parser.add_argument('-t', '--target', default=-1, help="target column")
 parser.add_argument(
     '-y', '--hyperparams', type=str, default='default', help="hyperparameter specification"
@@ -26,6 +25,15 @@ parser.add_argument(
 parser.add_argument('-s', '--score', type=str, default='sklearn', help="score")
 parser.add_argument(
     '-v', '--values', default='random', help="random, or number of values for grid search"
+)
+parser.add_argument(
+    '-i', '--iterations', type=int, default=2000, help="number of iterations per step"
+)
+parser.add_argument(
+    '-o', '--tolerance', type=int, default=3, help="number of deteriorating steps to tolerate"
+)
+parser.add_argument(
+    '-m', '--max-iterations', type=int, default=100000, help="max number of iterations"
 )
 parser.add_argument(
     '-c', '--classifier-iterations', type=int, default=1000, help="classifier training iterations"
@@ -85,11 +93,14 @@ def neg_ks_distance_score(synthesizer, synthesized):
 def sklearn_score(synthesizer, synthesized):
     train = synthesizer.preprocess(data=synthesized)
     estimator = GradientBoostingClassifier()
-    estimator.fit(X=train.drop(labels=target, axis=1), y=train[target])
-    test = synthesizer.preprocess(data=heldout.copy())
-    return roc_auc_scorer(
-        clf=estimator, X=test.drop(labels=target, axis=1), y=test[target]
-    )
+    try:
+        estimator.fit(X=train.drop(labels=target, axis=1), y=train[target])
+        test = synthesizer.preprocess(data=heldout.copy())
+        return roc_auc_scorer(
+            clf=estimator, X=test.drop(labels=target, axis=1), y=test[target]
+        )
+    except ValueError:
+        return None
 
 
 def tf_score(synthesizer, synthesized):
@@ -101,15 +112,23 @@ def tf_score(synthesizer, synthesized):
 
 def pessimistic_score(hyperparams, n_runs, scorer_fn):
     def _score():
+        history = [None for _ in range(args.tolerance)]
         with BasicSynthesizer(data=data, exclude_encoding_loss=True, **hyperparams) as synthesizer:
-            print(datetime.now().strftime('%H:%M:%S'), 'synthesis...', flush=True)
-            synthesizer.learn(num_iterations=args.iterations, data=original.copy())
-            synthesized = synthesizer.synthesize(n=10000)
-            print(datetime.now().strftime('%H:%M:%S'), 'computing score...', flush=True)
-            result = scorer_fn(synthesizer, synthesized)
-            print(datetime.now().strftime('%H:%M:%S'), 'synthetic score:', result, flush=True)
-            print()
-            return result
+            print(datetime.now().strftime('%H:%M:%S'), 'training...', flush=True)
+            for _ in range(args.max_iterations // args.iterations):
+                synthesizer.learn(num_iterations=args.iterations, data=original.copy())
+                synthesized = synthesizer.synthesize(n=10000)
+                print(datetime.now().strftime('%H:%M:%S'), 'computing score...', flush=True)
+                result = scorer_fn(synthesizer, synthesized)
+                print(datetime.now().strftime('%H:%M:%S'), 'synthetic score:', result, flush=True)
+                print()
+                history.pop(0)
+                history.append(result)
+                if any(score is None for score in history):
+                    continue
+                if all(history[n] < history[0] for n in range(1, len(history))):
+                    break
+            return max(history)
     return min([_score() for _ in range(n_runs)])
 
 
