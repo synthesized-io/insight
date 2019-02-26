d818f307e12a2c9944ac59158c3012328677f733
AlexKuhnle <alexkuhnle@t-online.de>
2019-02-26 11:09:12 +0000

    updated date value, fixed architecture issues

--- synthesized/core/values/date.py
+++ synthesized/core/values/date.py
@@ -8,35 +8,38 @@ from .continuous import ContinuousValue
 
 class DateValue(Value):
 
-    def __init__(self, name, embedding_size, start_date=None):
+    def __init__(self, name, capacity=None, embedding_size=None, start_date=None):
         super().__init__(name=name)
 
-        self.embedding_size = embedding_size
         self.start_date = start_date
 
         self.delta = self.add_module(
             module=ContinuousValue, name=self.name, positive=True
         )
         self.hour = self.add_module(
-            module=CategoricalValue, name=(self.name + '-hour'), categories=24,
+            module=CategoricalValue, name=(self.name + '-hour'), categories=24, capacity=capacity,
             embedding_size=embedding_size
         )
         self.dow = self.add_module(
-            module=CategoricalValue, name=(self.name + '-dow'), categories=7,
+            module=CategoricalValue, name=(self.name + '-dow'), categories=7, capacity=capacity,
             embedding_size=embedding_size
         )
         self.day = self.add_module(
-            module=CategoricalValue, name=(self.name + '-day'), categories=31,
+            module=CategoricalValue, name=(self.name + '-day'), categories=31, capacity=capacity,
             embedding_size=embedding_size
         )
         self.month = self.add_module(
-            module=CategoricalValue, name=(self.name + '-month'), categories=12,
+            module=CategoricalValue, name=(self.name + '-month'), categories=12, capacity=capacity,
             embedding_size=embedding_size
         )
 
     def specification(self):
         spec = super().specification()
-        spec.update(embedding_size=self.embedding_size)
+        spec.update(
+            delta=self.delta.specification(), hour=self.hour.specification(),
+            dow=self.dow.specification(), day=self.day.specification(),
+            month=self.month.specification()
+        )
         return spec
 
     def input_size(self):
@@ -46,12 +49,12 @@ class DateValue(Value):
     def output_size(self):
         return self.delta.output_size()
 
-    def trainable_labels(self):
-        yield from self.delta.trainable_labels()
-        yield from self.hour.trainable_labels()
-        yield from self.dow.trainable_labels()
-        yield from self.day.trainable_labels()
-        yield from self.month.trainable_labels()
+    def labels(self):
+        yield from self.delta.labels()
+        yield from self.hour.labels()
+        yield from self.dow.labels()
+        yield from self.day.labels()
+        yield from self.month.labels()
 
     def placeholders(self):
         yield from self.delta.placeholders()
@@ -61,12 +64,16 @@ class DateValue(Value):
         yield from self.month.placeholders()
 
     def extract(self, data):
+        # happens in identify_value
+        # data[self.name] = pd.to_datetime(data[self.name])
         if self.start_date is None:
-            self.start_date = data[self.name].astype(dtype='datetime64').min()
-        elif data[self.name].astype(dtype='datetime64').min() == self.start_date:
+            # self.start_date = data[self.name].astype(dtype='datetime64').min()
+            self.start_date = data[self.name].min()
+        # elif data[self.name].astype(dtype='datetime64').min() == self.start_date:
+        elif data[self.name].min() < self.start_date:
             raise NotImplementedError
 
-    def preprocess(self, data):
+    def encode(self, data):
         data[self.name] = data[self.name].astype(dtype='datetime64')
         data = data.sort_values(by=self.name)
         data[self.name + '-hour'] = data[self.name].dt.hour
@@ -84,15 +91,16 @@ class DateValue(Value):
         data[self.name] += self.start_date
         return data
 
-    def feature(self, x=None):
-        if x is None:
-            return tf.FixedLenFeature(shape=(), dtype=tf.float32, default_value=None)
-        else:
-            return tf.train.Feature(float_list=tf.train.FloatList(value=(x,)))
+    def features(self, x=None):
+        features = super().features(x=x)
+        features.update(self.delta.features(x=x))
+        features.update(self.hour.features(x=x))
+        features.update(self.dow.features(x=x))
+        features.update(self.day.features(x=x))
+        features.update(self.month.features(x=x))
+        return features
 
     def tf_input_tensor(self, feed=None):
-        # python function????
-        assert feed is None
         delta = self.delta.input_tensor(feed=feed)
         hour = self.hour.input_tensor(feed=feed)
         dow = self.dow.input_tensor(feed=feed)
@@ -102,7 +110,7 @@ class DateValue(Value):
         return x
 
     def tf_output_tensors(self, x):
-        return self.delta.output_tensor(x=x)
+        return self.delta.output_tensors(x=x)
 
     def tf_loss(self, x, feed=None):
         # skip last and assume absolute value
