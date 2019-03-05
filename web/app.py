dfc660a7587aab93e0b50d3e88890b3dcbbf1a60
Denis Borovikov <denis.borovikov@gmail.com>
2019-03-05 13:47:19 +0000

    Enable CORS

--- web/app.py
+++ web/app.py
@@ -11,6 +11,7 @@ from flask_bcrypt import Bcrypt
 from flask_jwt import JWT, jwt_required, current_identity
 from flask_restful import Resource, Api, reqparse, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from synthesized.core import BasicSynthesizer
 from synthesized.core.values import ContinuousValue
@@ -31,6 +32,7 @@ class JSONCompliantEncoder(JSONEncoder):
 
 
 app = Flask(__name__)
+CORS(app)
 app.json_encoder = JSONCompliantEncoder
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True  # this is crucial to enable handling of JWTError
