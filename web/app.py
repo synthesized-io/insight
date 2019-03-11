de32a03a887c5ac998008f2b4ba98828c5ca0620
Denis Borovikov <denis.borovikov@gmail.com>
2019-03-11 11:09:51 +0000

    Optimize import

--- web/app.py
+++ web/app.py
@@ -9,7 +9,7 @@ from io import StringIO, BytesIO
 import pandas as pd
 import simplejson
 from werkzeug.datastructures import FileStorage
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS
 from flask_restful import Resource, Api, reqparse, abort
