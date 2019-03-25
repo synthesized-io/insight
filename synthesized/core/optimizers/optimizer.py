fixed optimizer/gradient-related problem with gamma

synthesized/core/optimizers/optimizer.py
@@ -47,7 +47,10 @@ class Optimizer(Module):
             loss=loss, var_list=None, aggregation_method=None, colocate_gradients_with_ops=False,
             grad_loss=None  # gate_gradients=GATE_OP
         )
-        grads_and_vars = [(grad, var) for grad, var in grads_and_vars if grad is not None]
+        grads_and_vars = [
+            (tf.where(condition=tf.math.is_nan(x=grad), x=tf.zeros_like(tensor=grad), y=grad), var)
+            for grad, var in grads_and_vars if grad is not None
+        ]
         if gradient_norms:
             gradient_norms = dict()
             for grad, var in grads_and_vars:
@@ -61,9 +64,8 @@ class Optimizer(Module):
                     t=grad, clip_value_min=-self.clip_gradients, clip_value_max=self.clip_gradients
                 )
                 grads_and_vars[n] = (clipped_grad, var)
-        optimized = self.optimizer.apply_gradients(
-            grads_and_vars=grads_and_vars, global_step=Module.global_step
-        )
+        optimized = self.optimizer.apply_gradients(grads_and_vars=grads_and_vars)
+        # , global_step=Module.global_step  (incremented in synthesizer?!)
         if gradient_norms is False:
             return optimized
         else: