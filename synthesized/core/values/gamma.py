244f6aa50ca9e56ebd02fbd375d30736b8a23aa2
Nikolay Baldin <baldin.np@gmail.com>
2019-03-25 14:36:26 +0000

    Geolocation data for evaluation

--- synthesized/core/values/gamma.py
+++ synthesized/core/values/gamma.py
@@ -1,7 +1,9 @@
+from synthesized.core.module import tensorflow_name_scoped
 from .continuous import ContinuousValue
 from scipy.stats import norm
 from scipy.stats import gamma
-
+import tensorflow as tf
+import tensorflow_probability as tfp
 
 class GammaDistrValue(ContinuousValue):
 
@@ -42,3 +44,44 @@ class GammaDistrValue(ContinuousValue):
         data = data[data[self.name] != float('inf')]
         data = data[data[self.name] != float('-inf')]
         return super().postprocess(data=data)
+
+    @tensorflow_name_scoped
+    def output_tensors(self, x):
+        return super().output_tensors(x=x)
+
+    @tensorflow_name_scoped
+    def distribution_loss(self, samples):
+        temp = samples
+        samples = tf.squeeze(input=samples, axis=1)
+        tfd = tfp.distributions
+       # samples = tf.Print(samples, [samples])
+        dist_normlal = tfd.Normal(loc=0., scale=1.)
+        dist_gamma = tfd.Gamma(concentration=self.shape, rate=self.scale)
+        samples = dist_gamma.cdf(value = samples)
+        samples = dist_normlal.quantile(value = samples)
+        samples = tf.boolean_mask(samples, tf.is_finite(samples))
+        samples = tf.boolean_mask(samples, tf.math.logical_not(tf.is_nan(samples)))
+      #  samples = tf.Print(samples, [samples])
+
+        mean, variance = tf.nn.moments(x=samples, axes=0)
+        mean_loss = tf.squared_difference(x=mean, y=0.0)
+        variance_loss = tf.squared_difference(x=variance, y=1.0)
+
+        mean = tf.stop_gradient(input=tf.reduce_mean(input_tensor=samples, axis=0))
+        difference = samples - mean
+        squared_difference = tf.square(x=difference)
+        variance = tf.reduce_mean(input_tensor=squared_difference, axis=0)
+        third_moment = tf.reduce_mean(input_tensor=(squared_difference * difference), axis=0)
+        fourth_moment = tf.reduce_mean(input_tensor=tf.square(x=squared_difference), axis=0)
+        skewness = third_moment / tf.pow(x=variance, y=1.5)
+        kurtosis = fourth_moment / tf.square(x=variance)
+        num_samples = tf.cast(x=tf.shape(input=samples)[0], dtype=tf.float32)
+        # jarque_bera = num_samples / 6.0 * (tf.square(x=skewness) + \
+        #     0.25 * tf.square(x=(kurtosis - 3.0)))
+        jarque_bera = tf.square(x=skewness) + tf.square(x=(kurtosis - 3.0))
+        jarque_bera_loss = tf.squared_difference(x=jarque_bera, y=0.0)
+        loss = mean_loss + variance_loss + jarque_bera_loss
+        grads = tf.gradients(loss, [temp])
+        loss = tf.Print(loss, [grads, loss])
+
+        return loss
