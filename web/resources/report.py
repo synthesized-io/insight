946085ef2d1e7a227ad4e5fd3c04f41773f76cdd
Denis Borovikov <denis.borovikov@gmail.com>
2019-03-26 13:35:57 +0000

    Add correlation analysis

--- web/resources/report.py
+++ web/resources/report.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from operator import attrgetter
 from typing import Iterable
 
+import pandas as pd
 import simplejson
 from flask import current_app, jsonify
 from flask_jwt_extended import get_jwt_identity, jwt_required
@@ -10,9 +11,9 @@ from flask_restful import Resource, reqparse, abort
 
 from .common import DatasetAccessMixin
 from ..domain.analysis import DatasetMeta
+from ..domain.correlation import compute_correlation_similarity
 from ..domain.model import Report, ReportItem, ReportItemType, Dataset
 from ..domain.repository import Repository
-
 
 # Reports API
 #
@@ -21,6 +22,8 @@ from ..domain.repository import Repository
 # GET  /datasets/123/report-items/123
 # POST /datasets/123/reports-items/123/move
 # POST /datasets/123/reports-items/123/updatesettings
+
+MAX_SAMPLE_SIZE = 1000
 
 
 class ReportResource(Resource, DatasetAccessMixin):
@@ -151,9 +154,10 @@ class ReportItemsUpdateSettingsResource(Resource, DatasetAccessMixin):
     def __init__(self, **kwargs):
         self.dataset_repo: Repository = kwargs['dataset_repo']
         self.report_item_repo: Repository = kwargs['report_item_repo']
+        self.synthesis_repo: Repository = kwargs['synthesis_repo']
 
     def post(self, dataset_id, report_item_id):
-        self.get_dataset_authorized(dataset_id)
+        dataset = self.get_dataset_authorized(dataset_id)
 
         report_item: ReportItem = self.report_item_repo.get(report_item_id)
         if not report_item:
@@ -167,15 +171,37 @@ class ReportItemsUpdateSettingsResource(Resource, DatasetAccessMixin):
 
         if report_item.item_type == ReportItemType.CORRELATION:
             columns = settings['columns']
-            # TODO: compute correlations and save to `results`
+            syntheses = self.synthesis_repo.find_by_props({'dataset_id': dataset_id})
+            if len(syntheses) == 0:
+                abort(409, message='Not synthesis found for dataset: ' + str(dataset_id))
+            elif len(syntheses) > 1:
+                abort(409, message='There is more than one synthesis for dataset: ' + str(dataset_id))
+            else:
+                synthesis = syntheses[0]
+
+            df_orig = pd.read_csv(BytesIO(dataset.blob), encoding='utf-8')
+            df_synth = pd.read_csv(BytesIO(synthesis.blob), encoding='utf-8')
+
+            correlation_similarity = compute_correlation_similarity(df_orig, df_synth, columns)
+
+            size = min(len(df_orig), len(df_synth), MAX_SAMPLE_SIZE)
+            df_orig_sample = df_orig[columns].sample(size)
+            df_synth_sample = df_orig[columns].sample(size)
+
+            results = {
+                'correlation_similarity': correlation_similarity.to_dict(),
+                'original_sample': df_orig_sample.to_dict(orient='list'),
+                'synthetic_sample': df_synth_sample.to_dict(orient='list')
+            }
+            report_item.results = simplejson.dumps(results).encode('utf-8')
         elif report_item.item_type == ReportItemType.MODELLING:
             response_variavle = settings['response_variable']
             explanatory_variables = settings['explanatory_variables']
             model = settings['model']
             # TODO: run model and save results to `results`
 
-        settings_json = simplejson.dumps(settings).encode('utf-8')
-        report_item.settings = settings_json
+        settings_blob = simplejson.dumps(settings).encode('utf-8')
+        report_item.settings = settings_blob
         self.report_item_repo.save(report_item)
 
         return '', 204
