014c2e99b711a77304123f59ceb6400c817ff8f6
Denis Borovikov <denis.borovikov@gmail.com>
2019-04-11 18:12:12 +0100

    Supporting gender "U"

--- synthesized/core/values/person.py
+++ synthesized/core/values/person.py
@@ -18,9 +18,9 @@ class PersonValue(Value):
         self.firstname_label = firstname_label
         self.lastname_label = lastname_label
         self.email_label = email_label
-        # Assume the gender are always encoded like M or F
-        self.gender_mapping = {'M': 'male', 'F': 'female'}
-        self.title_mapping = {'M': 'Mr', 'F': 'Mrs'}
+        # Assume the gender are always encoded like M or F or U(???)
+        self.gender_mapping = {'M': 'male', 'F': 'female', 'U': 'female'}
+        self.title_mapping = {'M': 'Mr', 'F': 'Mrs', 'U': 'female'}
 
         if gender_label is None:
             self.gender = None
