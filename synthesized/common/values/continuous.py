3ac1c077ac91309ac46a52b4973d36c8157c7286
Denis Borovikov <denis.borovikov@gmail.com>
2019-07-09 19:16:32 +0100

    Use Quantile Transformer instead of cdf/ppf

--- synthesized/common/values/continuous.py
+++ synthesized/common/values/continuous.py
@@ -1,28 +1,13 @@
-from math import log
 from typing import Any, Dict, List, Tuple
 
+import numpy as np
 import pandas as pd
-from scipy.stats import gamma, gilbrat, gumbel_r, kstest, lognorm, norm, uniform, weibull_min
 import tensorflow as tf
+from sklearn.preprocessing import QuantileTransformer
 from tensorflow_probability import distributions as tfd
 
 from .value import Value
 from ..module import Module, tensorflow_name_scoped
-import numpy as np
-
-OUTLIERS_PERCENTILE = 0.01
-FITTING_SUBSAMPLE = 10000
-FITTING_INF_TOLERANCE = 0.01
-FITTING_THRESHOLD = 0.1
-DISTRIBUTIONS = dict(
-    # beta=(beta, tfd.Beta),
-    gamma=(gamma, tfd.Gamma),
-    gilbrat=(gilbrat, None),
-    gumbel=(gumbel_r, tfd.Gumbel),
-    log_normal=(lognorm, tfd.LogNormal),
-    uniform=(uniform, tfd.Uniform),
-    weibull=(weibull_min, None)
-)
 
 
 class ContinuousValue(Value):
@@ -31,7 +16,7 @@ class ContinuousValue(Value):
         self, name: str, weight: float,
         # Scenario
         integer: bool = None, positive: bool = None, nonnegative: bool = None,
-        distribution: str = None, distribution_params: Tuple[Any, ...] = None
+        transformer_n_quantiles=1000
     ):
         super().__init__(name=name)
 
@@ -40,10 +25,9 @@ class ContinuousValue(Value):
         self.integer = integer
         self.positive = positive
         self.nonnegative = nonnegative
-
-        assert distribution is None or distribution == 'normal' or distribution in DISTRIBUTIONS
-        self.distribution = distribution
-        self.distribution_params = distribution_params
+        self.distribution = None
+        self.distribution_params = None
+        self.transformer = QuantileTransformer(n_quantiles=transformer_n_quantiles, output_distribution='normal')
 
         self.pd_types: Tuple[str, ...] = ('f', 'i')
         self.pd_cast = (lambda x: pd.to_numeric(x, errors='coerce', downcast='integer'))
@@ -112,54 +96,7 @@ class ContinuousValue(Value):
             self.distribution_params = (column[0],)
             return
 
-        # positive / nonnegative transformation
-        if self.positive or self.nonnegative:
-            if self.nonnegative and not self.positive:
-                column = np.maximum(column, 0.001)
-            column = np.log(np.sign(column) * (1.0 - np.exp(-np.abs(column)))) + np.maximum(column, 0.0)
-
-        # remove outliers
-        percentile = [OUTLIERS_PERCENTILE * 50.0, 100.0 - OUTLIERS_PERCENTILE * 50.0]
-        lower, upper = np.percentile(column, percentile)
-        clean = column[(column >= lower) & (column <= upper)]
-        assert len(clean) >= 2
-        subsample = np.random.choice(clean, min(len(column), FITTING_SUBSAMPLE))
-
-        # normal distribution
-        self.distribution = 'normal'
-        mean = subsample.mean()
-        stddev = subsample.std()
-        if stddev == 0.0:
-            stddev = column.std()
-        self.distribution_params = (mean, stddev)
-        transformed = (subsample - mean) / stddev
-        min_distance, p = kstest(transformed, 'norm', N=10000)
-
-        # other distributions
-        for name, (distribution, _) in DISTRIBUTIONS.items():
-            distribution_params = distribution.fit(subsample)
-            transformed = norm.ppf(distribution.cdf(subsample, *distribution_params))
-
-            is_nan = np.isnan(transformed)
-            if is_nan.any():
-                assert is_nan.all()
-                continue
-
-            num_inf = (transformed == float('inf')).sum() + (transformed == float('-inf')).sum()
-            if num_inf / len(transformed) > FITTING_INF_TOLERANCE:
-                # print('INF TOLERANCE:', name, num_inf / len(transformed))
-                continue
-
-            distance, p = kstest(transformed, 'norm', N=10000)
-            if distance < min_distance:
-                # print('extract fit:', name, num_inf / len(transformed))
-                min_distance = distance
-                self.distribution = name
-                self.distribution_params = tuple(distribution_params)
-
-        # if distance > FITTING_THRESHOLD:
-        #     self.distribution = None
-        #     self.distribution_params = None
+        self.transformer.fit(df[self.name].values.reshape(-1, 1))
 
     def preprocess(self, df: pd.DataFrame) -> pd.DataFrame:
         # TODO: mb removal makes learning more stable (?), an investigation required
@@ -182,16 +119,7 @@ class ContinuousValue(Value):
                 np.sign(df[self.name]) * (1.0 - np.exp(-np.abs(df[self.name])))
             )
 
-        if self.distribution == 'normal':
-            assert self.distribution_params is not None
-            mean, stddev = self.distribution_params
-            df.loc[:, self.name] = (df[self.name] - mean) / stddev
-
-        elif self.distribution is not None:
-            df.loc[:, self.name] = norm.ppf(
-                DISTRIBUTIONS[self.distribution][0].cdf(df[self.name], *self.distribution_params)
-            )
-            df = df[(df[self.name] != float('inf')) & (df[self.name] != float('-inf'))]
+        df.loc[:, self.name] = self.transformer.transform(df[self.name].values.reshape(-1, 1))
 
         assert not df[self.name].isna().any()
         assert (df[self.name] != float('inf')).all() and (df[self.name] != float('-inf')).all()
@@ -203,26 +131,8 @@ class ContinuousValue(Value):
         if self.distribution == 'dirac':
             assert self.distribution_params is not None
             df.loc[:, self.name] = self.distribution_params[0]
-
         else:
-            if self.distribution == 'normal':
-                assert self.distribution_params is not None
-                mean, stddev = self.distribution_params
-                df.loc[:, self.name] = df[self.name] * stddev + mean
-
-            elif self.distribution is not None:
-                df.loc[:, self.name] = DISTRIBUTIONS[self.distribution][0].ppf(
-                    norm.cdf(df[self.name]), *self.distribution_params
-                )
-
-            if self.positive or self.nonnegative:
-                df.loc[:, self.name] = np.log(1 + np.exp(-np.abs(df[self.name]))) + \
-                                         np.maximum(df[self.name], 0.0)
-                if self.nonnegative and not self.positive:
-                    zeros = np.zeros_like(df[self.name])
-                    df.loc[:, self.name] = np.where(
-                        (df[self.name] >= 0.001), df[self.name], zeros
-                    )
+            df.loc[:, self.name] = self.transformer.inverse_transform(df[self.name].values.reshape(-1, 1))
 
         assert not df[self.name].isna().any()
         assert (df[self.name] != float('inf')).all() and (df[self.name] != float('-inf')).all()
@@ -278,57 +188,6 @@ class ContinuousValue(Value):
             return tf.constant(value=0.0, dtype=tf.float32)
 
         samples = ys[0]
-
-        if self.distribution == 'normal':
-            assert self.distribution_params is not None
-            loc, scale = self.distribution_params
-            distribution = tfd.Normal(loc=loc, scale=scale)
-            samples = distribution.cdf(value=samples)
-        elif self.distribution == 'gamma':
-            assert self.distribution_params is not None
-            shape, location, scale = self.distribution_params
-            distribution_class = DISTRIBUTIONS[self.distribution][1]
-            assert distribution_class is not None
-            distribution = distribution_class(concentration=shape, rate=1.0)
-            samples = tf.where(
-                condition=(samples < location), x=(samples + 2 * location), y=samples
-            )
-            samples = (samples - location) / scale
-            samples = distribution.cdf(value=samples) / scale
-        elif self.distribution == 'gumbel':
-            assert self.distribution_params is not None
-            location, scale = self.distribution_params
-            distribution_class = DISTRIBUTIONS[self.distribution][1]
-            assert distribution_class is not None
-            distribution = distribution_class(location=location, scale=scale)
-            samples = tf.where(
-                condition=(samples < location), x=(samples + 2 * location), y=samples
-            )
-            samples = distribution.cdf(value=samples)
-        elif self.distribution == 'log_normal':
-            assert self.distribution_params is not None
-            shape, location, scale = self.distribution_params
-            distribution_class = DISTRIBUTIONS[self.distribution][1]
-            assert distribution_class is not None
-            distribution = distribution_class(loc=log(scale), scale=scale)
-            samples = tf.where(
-                condition=(samples < location), x=(samples + 2 * location), y=samples
-            )
-            samples = samples - location
-            samples = distribution.cdf(value=samples)
-        elif self.distribution == 'weibull':
-            assert self.distribution_params is not None
-            shape, location, scale = self.distribution_params
-            distribution_class = DISTRIBUTIONS['gamma'][1]
-            assert distribution_class is not None
-            distribution = distribution_class(concentration=shape, rate=1.0)
-            samples = tf.where(
-                condition=(samples < location), x=(samples + 2 * location), y=samples
-            )
-            samples = (samples - location) / scale
-            samples = distribution.cdf(value=samples) / scale
-        else:
-            assert False
 
         samples = tf.boolean_mask(tensor=samples, mask=tf.math.logical_not(x=tf.is_nan(x=samples)))
         normal_distribution = tfd.Normal(loc=0.0, scale=1.0)
