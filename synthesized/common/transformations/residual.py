5d1eab479f32db3ec7e968db071f4f6bf998d99d
Denis Borovikov <denis.borovikov@gmail.com>
2019-07-16 11:33:22 +0100

    Fix NPE in repr method

--- synthesized/common/transformations/residual.py
+++ synthesized/common/transformations/residual.py
@@ -40,7 +40,7 @@ class ResidualTransformation(Transformation):
         spec = super().specification()
         spec.update(
             layers=[layer.specification() for layer in self.layers],
-            identity_transformation=self.identity_transformation.specification()
+            identity_transformation=(self.identity_transformation.specification() if self.identity_transformation else None)
         )
         return spec
 
