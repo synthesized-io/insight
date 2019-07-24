9db79099202e365d7f653c2b52df44f51d67a0b0
Denis Borovikov <denis.borovikov@gmail.com>
2019-07-24 11:34:46 +0100

    Clean-up the utility code

--- synthesized/testing/utility.py
+++ synthesized/testing/utility.py
@@ -99,16 +99,6 @@ class UtilityTesting:
                 ax = sns.countplot(x=col, hue='dataset', data=concatenated,
                                    palette={'orig': COLOR_ORIG, 'synth': COLOR_SYNTH}, ax=ax)
                 ax.set_xticklabels(ax.get_xticklabels(), rotation=15)
-            # elif dtype == DisplayType.CATEGORICAL_SIMILARITY:
-            #     # workaround for kde failing on datasets with only one value
-            #     if self.df_test[col].nunique() < 2 or self.df_synth[col].nunique() < 2:
-            #         kde = False
-            #     else:
-            #         kde = True
-            #     sns.distplot(self.df_test[col], color=COLOR_ORIG, label='orig', kde=kde, hist=True, norm_hist=True,
-            #                  hist_kws={"color": COLOR_ORIG}, ax=ax)
-            #     sns.distplot(self.df_synth[col], color=COLOR_SYNTH, label='synth', kde=kde, hist=True, norm_hist=True,
-            #                  hist_kws={"color": COLOR_SYNTH}, ax=ax)
             elif dtype == DisplayType.CONTINUOUS:
                 percentiles = [remove_outliers * 100. / 2, 100 - remove_outliers * 100. / 2]
                 start, end = np.percentile(self.df_test[col], percentiles)
@@ -214,10 +204,10 @@ class UtilityTesting:
             y_columns_new.append(y_column)
             if dtypes[y_column] != DisplayType.CATEGORICAL:
                 categorical_y_column = y_column + ' (categorical reduction)'
-                edges = bin_edges(df_orig[y_column])
-                df_orig[categorical_y_column] = to_categorical(df_orig[y_column], edges)
-                df_test[categorical_y_column] = to_categorical(df_test[y_column], edges)
-                df_synth[categorical_y_column] = to_categorical(df_synth[y_column], edges)
+                edges = UtilityTesting._bin_edges(df_orig[y_column])
+                df_orig[categorical_y_column] = UtilityTesting._to_categorical(df_orig[y_column], edges)
+                df_test[categorical_y_column] = UtilityTesting._to_categorical(df_test[y_column], edges)
+                df_synth[categorical_y_column] = UtilityTesting._to_categorical(df_synth[y_column], edges)
                 dtypes[categorical_y_column] = DisplayType.CATEGORICAL
                 y_columns_new.append(categorical_y_column)
                 y_orig_columns[categorical_y_column] = y_column
@@ -301,6 +291,15 @@ class UtilityTesting:
             'score_utility',
             'error_utility'
         ])
+
+    @staticmethod
+    def _bin_edges(a):
+        _, edges = np.histogram(a, bins=10)
+        return edges
+
+    @staticmethod
+    def _to_categorical(a, edges):
+        return np.digitize(a, edges)
 
     def compare_marginal_distributions(self, target_column, conditional_column, bins=4):
         if self.df_orig[conditional_column].dtype == 'O':
@@ -386,110 +385,3 @@ class UtilityTesting:
         start, end = np.percentile(self.df_orig[col], [2.5, 97.5])  # TODO parametrize
         plt.hist([self.df_orig[col], self.df_synth[col]], label=['orig', 'synth'], range=(start, end))
         plt.legend()
-
-
-def bin_edges(a):
-    _, edges = np.histogram(a, bins=10)
-    return edges
-
-
-def to_categorical(a, edges):
-    return np.digitize(a, edges)
-
-
-# Deprecated
-class TestingEnvironment:
-    def __init__(self):
-        self.synth_stat_models = [{"model": LogisticRegression, "variables": self.get_LogisticRegression_variables},
-                                  {"model": LinearRegression, "variables": self.get_LinearRegression_variables}]
-
-    def get_LogisticRegression_variables(self, dataset):
-        X = ["operation", "amount", "balance"]
-        y = ["type"]
-        dataset = dataset[X + y].dropna()
-        return {"X": dataset[X], "y": dataset[y]}
-
-    def get_LinearRegression_variables(self, dataset):
-        dataset['date'] = pd.to_datetime(dataset['date'])
-        account_ids = dataset["account_id"].unique()
-        operations = dataset["operation"].unique()
-        X = np.array([]).reshape(0, 5)
-        y = np.array([])
-        for account_id in account_ids:
-            user = dataset[dataset["account_id"] == account_id]
-            for month in range(1, 12):
-                X_local = np.array([])
-                user_local = user[user["date"].dt.month == month]
-                for operation in operations:
-                    X_local = np.append(X_local, user_local[user_local["operation"] == operation]["amount"].sum())
-                X = np.vstack((X, X_local))
-                next_month_df = user[(user["date"].dt.month == month + 1) & (user["operation"] == 2)]
-                y = np.concatenate((y, np.array([next_month_df["amount"].sum()])))
-        return {"X": X, "y": y}
-
-    def compare_empirical_densities(self, original_dataset, synthetic_dataset, target_field):
-        import plotly.figure_factory as ff
-        from plotly.offline import init_notebook_mode, iplot
-
-        init_notebook_mode()  # distribution of spending on the first month in the first category
-
-        assert (len(synthetic_dataset[0]) > target_field), "Field value is out of range for the synthesized data"
-        assert (len(original_dataset[0]) > target_field), "Field value is out of range for the real data"
-
-        fake_spendings = np.asarray([a[target_field] for a in synthetic_dataset])
-        true_spendings = np.asarray([a[target_field] for a in original_dataset])
-
-        fake_spendings = np.asarray([a for a in fake_spendings if a != 0])
-        true_spendings = np.asarray([a for a in true_spendings if a != 0])
-
-        print("The sum of values in the field %s of synthesized data is %s" % (target_field, sum(fake_spendings)))
-        print("The sum of values in the field %s of original data is %s" % (target_field, sum(true_spendings)))
-
-        assert (sum(fake_spendings) > 0), "zero value in the field in the synthesized dataset"
-        assert (sum(true_spendings) > 0), "zero value in the field in the read dataset"
-
-        init_notebook_mode(connected=True)
-        # Group data together
-        hist_data = [true_spendings, fake_spendings]
-
-        group_labels = ['Real data', 'Synthetic data']
-
-        # Create distplot with custom bin_size
-        fig = ff.create_distplot(hist_data, group_labels, bin_size=[.01, .01])
-
-        # Plot!
-        iplot(fig, filename='jupyter/basic_bar')
-
-    def get_the_closest_element(self, data_set, element):
-        minimal_distance = 100
-        the_closest_fake_user = 0
-        for user in data_set:
-            if np.sum(abs(element - user)) < minimal_distance:
-                the_closest_fake_user = user
-                minimal_distance = np.sum(abs(element - user))
-        return (the_closest_fake_user, minimal_distance)
-
-    def get_reconstruction_errors(self, original_dataset, original_dataset_batch, synthetic_dataset):
-        import plotly.figure_factory as ff
-        from plotly.offline import init_notebook_mode, iplot
-
-        init_notebook_mode()  # distribution of spending on the first month in the first category
-
-        init_notebook_mode(connected=True)
-
-        errors_synthetic = [self.get_the_closest_element(synthetic_dataset, original_dataset_batch[i])[1] for i in
-                            range(0, len(original_dataset_batch))]
-        errors_original = [self.get_the_closest_element(original_dataset, original_dataset_batch[i])[1] for i in
-                           range(0, len(original_dataset_batch))]
-
-        # Group data together
-        # errors_scaled = [error/31 for error in errors]
-        hist_data = [errors_original, errors_synthetic]
-
-        group_labels = ['Errors for real', 'Errors for synthetic']
-
-        # Create distplot with custom bin_size
-        fig = ff.create_distplot(hist_data, group_labels, bin_size=[.001, .001])
-
-        # Plot!
-        iplot(fig, filename='jupyter/basic_bar')
