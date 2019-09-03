465907b9464fecdc29cdc3a5794a413fd67a052f
Denis Borovikov <denis.borovikov@gmail.com>
2019-09-03 14:09:52 +0100

    Allow multiple evaluations per a notebook.

--- synthesized/testing/evaluation.py
+++ synthesized/testing/evaluation.py
@@ -1,54 +1,45 @@
-import collections
 import datetime
 import json
-import os
 from collections import OrderedDict
 
 import numpy as np
 
-EVALUATION_NAME = 'EVALUATION_NAME'
-EVALUATION_CONFIG_PATH = 'EVALUATION_CONFIG_PATH'
-EVALUATION_BRANCH = 'EVALUATION_BRANCH'
-EVALUATION_REVISION = 'EVALUATION_REVISION'
-
 
 class Evaluation:
-    def __init__(self, config_path=None, name=None, metrics_file='../metrics.jsonl'):
-        if name:
-            self.name = name
-        else:
-            self.name = os.environ[EVALUATION_NAME]
-
-        if config_path:
-            self.config_path = config_path
-        else:
-            self.config_path = os.environ[EVALUATION_CONFIG_PATH]
-
+    def __init__(self, branch, revision, group, metrics_file='../metrics.jsonl'):
+        self.branch = branch
+        self.revision = revision
+        self.group = group
         self.metrics_file = metrics_file
-
-        with open(self.config_path, 'r') as f:
-            configs = json.load(f, object_pairs_hook=collections.OrderedDict)
-            self.config = configs['instances'][self.name]
         self.metrics = OrderedDict()
+        self.configs = OrderedDict()
 
-    def __setitem__(self, key, value):
-        if key in self.metrics:
-            self.metrics[key].append(value)
+    def record_config(self, evaluation, config):
+        self.configs[evaluation] = config
+
+    def record_metric(self, evaluation, key, value):
+        if evaluation not in self.metrics:
+            self.metrics[evaluation] = OrderedDict()
+        evaluation_metrics = self.metrics[evaluation]
+        if key in evaluation_metrics:
+            evaluation_metrics[key].append(value)
         else:
-            self.metrics[key] = [value]
+            evaluation_metrics[key] = [value]
 
     def write_metrics(self):
         timestamp = datetime.datetime.now().isoformat()
         with open(self.metrics_file, 'a') as f:
-            data = OrderedDict()
-            data['evaluation'] = self.name
-            data['branch'] = os.environ.get(EVALUATION_BRANCH, 'n/a')
-            data['revision'] = os.environ.get(EVALUATION_REVISION, 'n/a')
-            data['timestamp'] = timestamp
-            data['config'] = json.dumps(self.config)
-            for name, vals in self.metrics.items():
-                data[name + '_mean'] = np.mean(vals)
-                data[name + '_std'] = np.std(vals)
-                data[name + '_count'] = len(vals)
-            json.dump(data, f)
-            f.write('\n')
+            for evaluation in self.metrics.keys():
+                data = OrderedDict()
+                data['evaluation'] = evaluation
+                data['branch'] = self.branch
+                data['revision'] = self.revision
+                data['timestamp'] = timestamp
+                if evaluation in self.configs:
+                    data['config'] = json.dumps(self.configs[evaluation])
+                for name, vals in self.metrics.items():
+                    data[name + '_mean'] = np.mean(vals)
+                    data[name + '_std'] = np.std(vals)
+                    data[name + '_count'] = len(vals)
+                json.dump(data, f)
+                f.write('\n')
