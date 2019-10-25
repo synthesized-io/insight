6df19a8f1ec691a4447e1dc160d4f1b8e5435a13
Denis Borovikov <denis.borovikov@gmail.com>
2019-10-25 16:41:06 +0100

    Round floats before comparing

--- synthesized/highdim/highdim.py
+++ synthesized/highdim/highdim.py
@@ -29,7 +29,9 @@ class Sanitizer:
 
 
 class DefaultSanitizer(Sanitizer):
-    """The default implementation. Naively drops duplicates."""
+    """The default implementation. Drops duplicates. Floats are rounded."""
+
+    FLOAT_DECIMAL = 5
 
     def __init__(self, df_original: pd.DataFrame) -> None:
         self.df_original = df_original
@@ -37,10 +39,18 @@ class DefaultSanitizer(Sanitizer):
     def sanitize(self, df_synthesized: pd.DataFrame) -> pd.DataFrame:
         """Drop rows in df_synthesized that are present in df_original."""
 
-        original_rows = {row for row in self.df_original.itertuples(index=False)}
+        def normalize_tuple(nt):
+            res = []
+            for field in nt:
+                if isinstance(field, float):
+                    field = round(field, DefaultSanitizer.FLOAT_DECIMAL)
+                res.append(field)
+            return tuple(res)
+
+        original_rows = {normalize_tuple(row) for row in self.df_original.itertuples(index=False)}
         to_drop = []
         for i, row in enumerate(df_synthesized.itertuples(index=False)):
-            if row in original_rows:
+            if normalize_tuple(row) in original_rows:
                 to_drop.append(i)
         return df_synthesized.drop(to_drop)
 
