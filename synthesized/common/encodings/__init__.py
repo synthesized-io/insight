3cd3005c9f2dadedb8eeee32ac299aa3fae1a4e9
Simon Swan <shk.swan@gmail.com>
2020-01-22 15:18:57 +0000

    Dont import/register RnnVariationalEncoding

--- synthesized/common/encodings/__init__.py
+++ synthesized/common/encodings/__init__.py
@@ -1,10 +1,8 @@
 from .encoding import Encoding
-from .rnn_variational import RnnVariationalEncoding
 from .variational import VariationalEncoding
 from ..module import register
 
 
-register(name='rnn_variational', module=RnnVariationalEncoding)
 register(name='variational', module=VariationalEncoding)
 
 __all__ = ['Encoding']
