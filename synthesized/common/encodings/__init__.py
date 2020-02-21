f483156abdd821effd3fe5938988a8cf6e9adea6
Simon Swan <shk.swan@gmail.com>
2020-02-21 12:42:19 +0000

    Add new encoding to encodings/__init__.py

--- synthesized/common/encodings/__init__.py
+++ synthesized/common/encodings/__init__.py
@@ -1,19 +1,25 @@
+from typing import Dict, Type
+
 from .encoding import Encoding
+from .recurrent_deep_state_encoding import RecurrentDSSEncoding
 from .variational import VariationalEncoding
 from .variational_lstm import VariationalLSTMEncoding
 from .variational_recurrent_encoding import VariationalRecurrentEncoding
 from ..module import register
 
 
+register(name='recurrent_dss_encoding', module=RecurrentDSSEncoding)
 register(name='variational', module=VariationalEncoding)
 register(name='variational_lstm', module=VariationalLSTMEncoding)
 register(name='vrae', module=VariationalRecurrentEncoding)
 
-__all__ = ['Encoding', 'VariationalEncoding', 'VariationalLSTMEncoding', 'VariationalRecurrentEncoding']
-
-Encodings = {
+Encodings: Dict[str, Type[Encoding]] = {
     'encoding': Encoding,
-    'variational_encoding': VariationalEncoding,
-    'VariationalLSTMEncoding': VariationalLSTMEncoding,
-    'VariationalRecurrentEncoding': VariationalRecurrentEncoding
+    'recurrent_dss': RecurrentDSSEncoding,
+    'variational': VariationalEncoding,
+    'variational_lstm': VariationalLSTMEncoding,
+    'vrae': VariationalRecurrentEncoding
 }
+
+__all__ = ['Encoding', 'RecurrentDSSEncoding', 'VariationalEncoding', 'VariationalLSTMEncoding',
+           'VariationalRecurrentEncoding', 'Encodings']
