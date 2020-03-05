f6b3ac0557819394064a85a51cd040193f6a8be7
Amandla Mabona <amandla.mabona@cl.cam.ac.uk>
2020-03-05 12:53:00 +0200

    Make multiple time series short than single time series.

--- make_synthetic_time_series.py
+++ make_synthetic_time_series.py
@@ -5,11 +5,11 @@ import synthesized.testing.synthetic_distributions as dist
 
 
 def main(output_dir: str = "data/time-series/synthetic",
-         length: int = 1000, cycle_amplitude: float = 10.,
-         cycle_period: float = 125, trend_slope: float = 2,
-         trend_bias: float = 2, cont_ar_bias: float = 2,
-         cont_ar_order: int = 10, cont_ar_acf: float = 2.,
-         n_classes: int = 20,
+         length: int = 1000, short_length: int = 200,
+         cycle_amplitude: float = 10., cycle_period: float = 125,
+         trend_slope: float = 2, trend_bias: float = 2,
+         cont_ar_bias: float = 2, cont_ar_order: int = 10,
+         cont_ar_acf: float = 2., n_classes: int = 20,
          noise_sd: float = 1, num_series=10):
     # Make continuous data
     # -- Cyclical
@@ -41,7 +41,7 @@ def main(output_dir: str = "data/time-series/synthetic",
         local_data = dist.create_time_series_data(func=dist.continuous_auto_regressive(phi=phi,
                                                                                  c=cont_ar_bias,
                                                                                  sd=noise_sd),
-                                                  length=length)
+                                                  length=short_length)
         local_data["id"] = i
         data_frames.append(local_data)
     data = pd.concat(data_frames)
@@ -59,7 +59,7 @@ def main(output_dir: str = "data/time-series/synthetic",
     for i in range(num_series):
         local_data = dist.create_time_series_data(func=dist.categorical_auto_regressive(n_classes=n_classes,
                                                                                         sd=noise_sd),
-                                                  length=length)
+                                                  length=short_length)
         local_data["id"] = i
         data_frames.append(local_data)
     data = pd.concat(data_frames)
