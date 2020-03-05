16a7083b876df3f1886ebac0efa39b15ea1b4f1f
Amandla Mabona <amandla.mabona@cl.cam.ac.uk>
2020-03-05 13:57:22 +0200

    Stop synthetic AR process from diverging.

--- make_synthetic_time_series.py
+++ make_synthetic_time_series.py
@@ -9,7 +9,7 @@ def main(output_dir: str = "data/time-series/synthetic",
          cycle_amplitude: float = 10., cycle_period: float = 125,
          trend_slope: float = 2, trend_bias: float = 2,
          cont_ar_bias: float = 2, cont_ar_order: int = 10,
-         cont_ar_acf: float = 2., n_classes: int = 20,
+         cont_ar_acf: float = -0.75, n_classes: int = 20,
          noise_sd: float = 1, num_series=10):
     # Make continuous data
     # -- Cyclical
@@ -27,7 +27,8 @@ def main(output_dir: str = "data/time-series/synthetic",
     data.to_csv(f"{output_dir}/continuous-trend.csv", index=False)
 
     # -- Auto-correlation
-    phi = cont_ar_acf*np.ones(cont_ar_order)
+    phi = np.zeros(cont_ar_order)
+    phi[-1] = cont_ar_acf
     data = dist.create_time_series_data(func=dist.continuous_auto_regressive(phi=phi,
                                                                              c=cont_ar_bias,
                                                                              sd=noise_sd),
@@ -35,7 +36,8 @@ def main(output_dir: str = "data/time-series/synthetic",
     data.to_csv(f"{output_dir}/continuous-autoregressive.csv", index=False)
 
     # -- Number of series
-    phi = cont_ar_acf*np.ones(cont_ar_order)
+    phi = np.zeros(cont_ar_order)
+    phi[-1] = cont_ar_acf
     data_frames = []
     for i in range(num_series):
         local_data = dist.create_time_series_data(func=dist.continuous_auto_regressive(phi=phi,
