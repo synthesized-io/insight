9b6203f6cd7b231d2f8742aae03943756edf1e4e
Simon Swan <shk.swan@gmail.com>
2020-06-15 16:31:06 +0100

    Fix unresolved references

--- synthesized/common/values/__init__.py
+++ synthesized/common/values/__init__.py
@@ -1,21 +1,18 @@
-from .address import AddressValue
 from .associated_categorical import AssociatedCategoricalValue
-from .bank_number import BankNumberValue
 from .categorical import CategoricalValue
-from .compound_address import CompoundAddressValue
 from .continuous import ContinuousValue
 from .date import DateValue
 from .decomposed_continuous import DecomposedContinuousValue
 from .factory import ValueFactory, TypeOverride, ValueFactoryConfig, ValueFactoryWrapper
 from .identifier import IdentifierValue
 from .nan import NanValue
-from .person import PersonValue
 from .rule import RuleValue
 from .value import Value
 from .value_operations import ValueOps
 
 
-__all__ = ['AddressValue', 'AssociatedCategoricalValue', 'CategoricalValue', 'CompoundAddressValue', 'ContinuousValue',
-           'DateValue', 'DecomposedContinuousValue', 'IdentifierValue',
-           'NanValue', 'PersonValue', 'BankNumberValue', 'RuleValue',  'Value',
-           'ValueFactory', 'ValueFactoryWrapper', 'TypeOverride', 'ValueOps', 'ValueFactoryConfig']
+__all__ = [
+    'AssociatedCategoricalValue', 'CategoricalValue', 'ContinuousValue', 'DateValue', 'DecomposedContinuousValue',
+    'IdentifierValue', 'NanValue', 'RuleValue',  'Value',  'ValueFactory', 'ValueFactoryWrapper', 'TypeOverride',
+    'ValueOps', 'ValueFactoryConfig'
+]
