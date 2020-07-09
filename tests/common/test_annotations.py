fb822e92c383f1993d963c3666c3c23790567a5b
tonbadal <ton_badal@hotmail.com>
2020-07-09 13:24:11 +0200

    add addresses from file test

--- tests/common/test_annotations.py
+++ tests/common/test_annotations.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from synthesized import HighDimSynthesizer, MetaExtractor
-from synthesized.config import AddressParams, BankParams, PersonParams
+from synthesized.config import AddressParams, BankParams, PersonParams, AddressMetaConfig
 
 
 def test_annotations():
@@ -36,6 +36,34 @@ def test_annotations():
         address_params=address_params,
         bank_params=bank_params,
         person_params=person_params
+    )
+
+    with HighDimSynthesizer(df_meta=df_meta) as synthesizer:
+        synthesizer.learn(df_train=data, num_iterations=10)
+        df_synthesized = synthesizer.synthesize(num_rows=len(data))
+
+    assert df_synthesized.shape == data.shape
+
+
+def test_addresses_from_file():
+    data = pd.read_csv('data/annotations_nd.csv')
+
+    address_meta_config = AddressMetaConfig(addresses_file='')
+
+    address_params = AddressParams(
+        postcode_label='POSTCODE',
+        county_label='COUNTY',
+        city_label='POSTTOWN',
+        district_label='DISTRICT',
+        street_label='STREET',
+        house_number_label='HOUSENUMBER',
+        flat_label='FLAT',
+        house_name_label='HOUSENAME'
+    )
+
+    df_meta = MetaExtractor.extract(
+        df=data,
+        address_params=address_params
     )
 
     with HighDimSynthesizer(df_meta=df_meta) as synthesizer:
