afb235e83d36ec8c40ae37a5cdfb7ea0f20de9d1
Simon Swan <shk.swan@gmail.com>
2020-08-07 14:33:53 +0100

    SD-868 add test

--- tests/common/test_data_imputer.py
+++ tests/common/test_data_imputer.py
@@ -55,3 +55,23 @@ def test_continuous_outliers_imputation():
 
     data_imputer.impute_outliers(df_original, inplace=True, outliers_percentile=0.05)
     assert np.sum(df_original['x'].values > 10) == 0
+
+
+def test_mixed_dtypes_nan_imputation():
+    num_iterations = 50
+
+    df_original = pd.read_csv("data/credit.csv")
+    df_meta = MetaExtractor.extract(df=df_original)
+    synthesizer = HighDimSynthesizer(df_meta=df_meta)
+    synthesizer.__enter__()
+    synthesizer.learn(num_iterations=num_iterations, df_train=df_original)
+    df_synthesized = synthesizer.synthesize(num_rows=10)
+    print(df_original.shape, df_synthesized.shape)  # (9999, 11) (10, 11)
+    print([df_original[col].dtype for col in df_original.columns])
+
+    data_imputer = DataImputer(synthesizer)
+    data_imputer.impute_nans(df_original)  # TypeError: Cannot do inplace boolean setting on mixed-types with a non np.nan value
+
+    df = pd.concat([df_original, df_synthesized])
+    print(df.shape)  # (10009, 11)
+    data_imputer.impute_nans(df)  # RecursionError: maximum recursion depth exceeded
