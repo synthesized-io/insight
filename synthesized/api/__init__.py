6cd35777f0c2dc79467be2cf091ccff543ebe58c
Simon Swan <shk.swan@gmail.com>
2020-08-12 16:15:57 +0100

    SD-815 add meta_extractor methods

--- synthesized/api/__init__.py
+++ synthesized/api/__init__.py
@@ -1,4 +1,8 @@
-from .binary_builder import BinaryType, CompressionType, Binary, ModelBinary, DatasetBinary
+from .binary_builder import BinaryType
+from .binary_builder import CompressionType
+from .binary_builder import Binary
+from .binary_builder import ModelBinary
+from .binary_builder import DatasetBinary
 from .conditional import ConditionalSampler
 from .data_frame_meta import DataFrameMeta
 from .data_imputer import DataImputer
--- synthesized/api/meta_extractor.py
+++ synthesized/api/meta_extractor.py
@@ -1,5 +1,4 @@
 from typing import Dict, List, Union
-from enum import Enum
 
 import pandas as pd
 
@@ -9,15 +8,7 @@ from ..config import AddressParams
 from ..config import BankParams
 from ..config import CompoundAddressParams
 from ..config import PersonParams
-from ..metadata import MetaExtractor as _MetaExtractor
-
-
-class TypeOverride(Enum):
-    ID = 'ID'
-    DATE = 'DATE'
-    CATEGORICAL = 'CATEGORICAL'
-    CONTINUOUS = 'CONTINUOUS'
-    ENUMERATION = 'ENUMERATION'
+from ..metadata import MetaExtractor as _MetaExtractor, TypeOverride
 
 
 class MetaExtractor:
@@ -26,26 +17,73 @@ class MetaExtractor:
 
     @classmethod
     def extract(
-            cls, df: pd.DataFrame, config: MetaExtractorConfig = MetaExtractorConfig(),
-            id_index: str = None, time_index: str = None,
-            column_aliases: Dict[str, str] = None, associations: Dict[str, List[str]] = None,
+            cls,
+            df: pd.DataFrame,
+            config: MetaExtractorConfig = MetaExtractorConfig(),
+            id_index: str = None,
+            time_index: str = None,
+            column_aliases: Dict[str, str] = None,
+            associations: Dict[str, List[str]] = None,
             type_overrides: Dict[str, TypeOverride] = None,
-            find_rules: Union[str, List[str]] = None, produce_nans_for: List[str] = None,
+            find_rules: Union[str, List[str]] = None,
+            produce_nans_for: List[str] = None,
             produce_infs_for: List[str] = None,
-            address_params: AddressParams = None, bank_params: BankParams = None,
+            address_params: AddressParams = None,
+            bank_params: BankParams = None,
             compound_address_params: CompoundAddressParams = None,
-            person_params: PersonParams = None
+            person_params: PersonParams = None,
     ) -> DataFrameMeta:
-        pass
+
+        df_meta = DataFrameMeta()
+        df_meta._df_meta = _MetaExtractor.extract(
+            df=df,
+            config=config,
+            id_index=id_index,
+            time_index=time_index,
+            column_aliases=column_aliases,
+            associations=associations,
+            type_overrides=type_overrides,
+            find_rules=find_rules,
+            produce_nans_for=produce_nans_for,
+            produce_infs_for=produce_infs_for,
+            address_params=address_params,
+            bank_params=bank_params,
+            compound_address_params=compound_address_params,
+            person_params=person_params,
+        )
+        return df_meta
 
     def extract_dataframe_meta(
-            self, df: pd.DataFrame, id_index: str = None, time_index: str = None,
-            column_aliases: Dict[str, str] = None, associations: Dict[str, List[str]] = None,
+            self,
+            df: pd.DataFrame,
+            id_index: str = None,
+            time_index: str = None,
+            column_aliases: Dict[str, str] = None,
+            associations: Dict[str, List[str]] = None,
             type_overrides: Dict[str, TypeOverride] = None,
-            find_rules: Union[str, List[str]] = None, produce_nans_for: List[str] = None,
+            find_rules: Union[str, List[str]] = None,
+            produce_nans_for: List[str] = None,
             produce_infs_for: List[str] = None,
-            address_params: AddressParams = None, bank_params: BankParams = None,
+            address_params: AddressParams = None,
+            bank_params: BankParams = None,
             compound_address_params: CompoundAddressParams = None,
-            person_params: PersonParams = None
+            person_params: PersonParams = None,
     ) -> DataFrameMeta:
-        pass
+
+        df_meta = DataFrameMeta()
+        df_meta._df_meta = self._meta_extractor.extract(
+            df=df,
+            id_index=id_index,
+            time_index=time_index,
+            column_aliases=column_aliases,
+            associations=associations,
+            type_overrides=type_overrides,
+            find_rules=find_rules,
+            produce_nans_for=produce_nans_for,
+            produce_infs_for=produce_infs_for,
+            address_params=address_params,
+            bank_params=bank_params,
+            compound_address_params=compound_address_params,
+            person_params=person_params,
+        )
+        return df_meta
