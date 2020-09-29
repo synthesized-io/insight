921b2ae8ca02be5bf8ef6b4772109ccb9387c759
Simon Swan <shk.swan@gmail.com>
2020-09-29 11:09:05 +0100

    SD-1012 Add dataframe loading tests.

---
+++ tests/metadata/test_data_frame.py
@@ -0,0 +1,32 @@
+import logging
+
+import pandas as pd
+import pytest
+
+from synthesized import MetaExtractor, DataFrameMeta
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.fast
+def test_load_data_frame():
+
+    df = pd.read_csv('data/unittest.csv')
+    df_meta = MetaExtractor.extract(df=df)
+    meta_dict = df_meta.get_variables()
+    df_meta2 = DataFrameMeta.from_dict(meta_dict)
+
+    assert len(df_meta2.values) == len(df_meta.values)
+
+
+@pytest.mark.fast
+def test_load_data_frame_associations():
+    association_dict = {"NumberOfTimes90DaysLate": ["NumberOfTime60-89DaysPastDueNotWorse"]}
+
+    df = pd.read_csv('data/unittest.csv')
+    df_meta = MetaExtractor.extract(df=df, associations=association_dict)
+    meta_dict = df_meta.get_variables()
+
+    df_meta2 = DataFrameMeta.from_dict(meta_dict)
+    assert df_meta2.association_meta is not None
+    assert df_meta2.association_meta.binding_mask is not None
