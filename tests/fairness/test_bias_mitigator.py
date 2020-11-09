8c2f3cf6474d1ba9c4007c75a306109884e51db0
tonbadal <ton_badal@hotmail.com>
2020-11-09 12:14:11 +0000

    exagerate biases for tests

--- tests/fairness/test_bias_mitigator.py
+++ tests/fairness/test_bias_mitigator.py
@@ -9,7 +9,7 @@ from synthesized.insight.fairness import BiasMitigator, FairnessScorer
 from synthesized.testing.utils import testing_progress_bar
 
 
-def generate_biased_data(n: int = 1000, nans_prop: Optional[float] = 0.25):
+def generate_biased_data(n: int = 5000, nans_prop: Optional[float] = 0.05):
     x = np.random.randn(n)
     y = x + 0.5 * np.random.randn(n)
     x_nans = np.where(np.random.rand(n) < nans_prop, np.nan, np.random.randn(n)) if nans_prop else np.random.randn(n)
@@ -17,8 +17,8 @@ def generate_biased_data(n: int = 1000, nans_prop: Optional[float] = 0.25):
     df = pd.DataFrame({
         'x_nans': x_nans,
         'age': [int(abs(xx * 15 + 50)) for xx in x],
-        'gender': ['M' if (yy + np.random.randn()) > 0.5 else 'F' for yy in y],
-        'income': [int(abs(yy * 10_000 + 40_000)) for yy in y]
+        'gender': ['M' if (yy + np.random.randn() * 0.5) > 0.5 else 'F' for yy in y],
+        'income': [int(abs(yy * 5_000 + 40_000)) for yy in y]
     })
     return df
 
@@ -49,8 +49,7 @@ def test_bias_mitigator(n_bins):
     # Test from_dataframe()
     bias_mitigator = BiasMitigator.from_dataframe(synthesizer, df=df, sensitive_attrs=sensitive_attrs, target=target,
                                                   n_bins=n_bins)
-    df_unbiased = bias_mitigator.mitigate_biases_by_chunks(df, chunk_size=5, marginal_softener=0.25,
-                                                           n_loops=50, progress_callback=testing_progress_bar,
+    df_unbiased = bias_mitigator.mitigate_biases_by_chunks(df, n_loops=50, progress_callback=testing_progress_bar,
                                                            produce_nans=False)
 
     fs_f = FairnessScorer(df_unbiased, sensitive_attrs=sensitive_attrs, target=target)
