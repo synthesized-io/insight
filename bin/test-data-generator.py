363cfd15eadade644eaa36b3880169e40c45c3d3
tonbadal <ton_badal@hotmail.com>
2020-11-17 17:59:13 +0000

    separate executer from testing data generator class

---
+++ bin/test-data-generator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+
+from synthesized.complex import TestingDataGenerator
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Testing Data Generator.")
+    parser.add_argument("--config", dest="config", required=True,
+                        help="YAML configuration file.")
+    parser.add_argument("--output-file", dest="output_file", required=True,
+                        help="Output file.")
+    parser.add_argument("-n", "--num-rows", dest="num_rows", required=True, type=int,
+                        help="Number of rows to generate.")
+    parser.add_argument("--output-format", dest="output_format", required=False,
+                        default="csv", help="")
+    args = parser.parse_args()
+
+    generator = TestingDataGenerator.from_yaml(args.config)
+    df_synthesized = generator.synthesize(args.num_rows)
+
+    if args.output_format == "csv":
+        df_synthesized.to_csv(args.output_file)
+    elif args.output_format == "json":
+        df_synthesized.to_json(args.output_file, orient='records', indent=2)
+
+
+if __name__ == '__main__':
+    main()
--- synthesized/complex/testing_data_generator.py
+++ synthesized/complex/testing_data_generator.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import pandas as pd
 import yaml
@@ -12,47 +12,49 @@ class TestingDataGenerator:
         self.df_meta = df_meta
 
     @classmethod
-    def from_dict(cls, value_kwargs: List[Dict[str, Any]], groups_kwargs: List[Dict[str, Any]]):
+    def from_dict(cls, value_kwargs: Optional[List[Dict[str, Any]]], groups_kwargs: Optional[List[Dict[str, Any]]]):
         value_meta_dict = {vm.__name__.lower()[:-4]: vm for vm in ValueMeta.__subclasses__()}
 
         values = []
         # Values
-        for value_kwargs_i in value_kwargs:
-            value_type = value_kwargs_i['type']
-            value_cls = value_meta_dict[value_type]
+        if value_kwargs:
+            for value_kwargs_i in value_kwargs:
+                value_type = value_kwargs_i['type']
+                value_cls = value_meta_dict[value_type]
 
-            value = value_cls.from_dict(value_kwargs_i)
-            if len(value.learned_output_columns()) > 0:
-                raise ValueError(f"TestingDataGenerator not compatible with '{value_type}'")
-
-            values.append(value)
-
-        # Groups (various values with same parameters)
-        for group_kwargs_i in groups_kwargs:
-            group_type = group_kwargs_i['type']
-            value_cls = value_meta_dict[group_type]
-
-            group_name = group_kwargs_i.pop('name', None)
-            group_count = group_kwargs_i.pop('count', None)
-            group_names = group_kwargs_i.pop('names', None)
-
-            # Given 'name_format' and 'count'
-            if group_name and group_count:
-                names = [group_name.format(str(i)) for i in range(group_count)]
-
-            # Given 'names'
-            elif group_names:
-                names = group_names
-
-            else:
-                raise ValueError("Both 'group_name' and 'group_count', or 'group_names' must be given")
-
-            for name in names:
-                value = value_cls.from_dict(group_kwargs_i, name=name)
+                value = value_cls.from_dict(value_kwargs_i)
                 if len(value.learned_output_columns()) > 0:
-                    raise ValueError(f"TestingDataGenerator not compatible with '{group_type}'")
+                    raise ValueError(f"TestingDataGenerator not compatible with '{value_type}'")
 
                 values.append(value)
+
+        # Groups (various values with same parameters)
+        if groups_kwargs:
+            for group_kwargs_i in groups_kwargs:
+                group_type = group_kwargs_i['type']
+                value_cls = value_meta_dict[group_type]
+
+                group_name = group_kwargs_i.pop('name', None)
+                group_count = group_kwargs_i.pop('count', None)
+                group_names = group_kwargs_i.pop('names', None)
+
+                # Given 'name_format' and 'count'
+                if group_name and group_count:
+                    names = [group_name.format(str(i)) for i in range(group_count)]
+
+                # Given 'names'
+                elif group_names:
+                    names = group_names
+
+                else:
+                    raise ValueError("Both 'group_name' and 'group_count', or 'group_names' must be given")
+
+                for name in names:
+                    value = value_cls.from_dict(group_kwargs_i, name=name)
+                    if len(value.learned_output_columns()) > 0:
+                        raise ValueError(f"TestingDataGenerator not compatible with '{group_type}'")
+
+                    values.append(value)
 
         all_names = [value.name for value in values]
         duplicated_names = [name for name, counts in Counter(all_names).items() if counts > 1]
@@ -62,28 +64,23 @@ class TestingDataGenerator:
         df_meta = DataFrameMeta(values=values)
         return cls(df_meta)
 
-    def synthesize(self, num_rows: int) -> pd.DataFrame:
-        return self.df_meta.postprocess(pd.DataFrame([[], ] * num_rows))
-
     @classmethod
-    def generate_data_from_yaml(cls, file_name):
+    def from_yaml(cls, config_file_name: str):
 
-        with open(file_name, 'r') as fp:
+        with open(config_file_name, 'r') as fp:
             kwargs = yaml.safe_load(fp)
 
-        num_rows, output_file, values = cls.validate_arguments(["num_rows", "output_file", "values"], kwargs)
+        values = kwargs.get("values", None)
         groups = kwargs.get("groups", {})
         generator = cls.from_dict(values, groups)
 
-        df_synthesized = generator.synthesize(num_rows)
-        output_format = kwargs.get("output_format", "csv")
-        if output_format == "csv":
-            df_synthesized.to_csv(output_file)
-        elif output_format == "json":
-            df_synthesized.to_json(output_file, orient='records', indent=2)
+        return generator
+
+    def synthesize(self, num_rows: int) -> pd.DataFrame:
+        return self.df_meta.postprocess(pd.DataFrame([[], ] * num_rows))
 
     @staticmethod
-    def validate_arguments(args, kwargs) -> List:
+    def validate_arguments(args: List[str], kwargs: Dict[str, Any]) -> List:
         values = []
         for arg in args:
             if arg not in kwargs:
