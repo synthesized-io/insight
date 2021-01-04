fc0b2c6d8d7af4d6c6fd4d3eddf6a720aac4010a
tonbadal <ton_badal@hotmail.com>
2021-01-04 16:21:53 +0100

    add recursive break + test

--- synthesized/util.py
+++ synthesized/util.py
@@ -1,3 +1,7 @@
 def get_all_subclasses(cls):
     """Recursively get all subclasses"""
-    return cls.__subclasses__() + [s for c in cls.__subclasses__() for s in get_all_subclasses(c)]
+    subclasses = cls.__subclasses__()
+    for sc in subclasses:
+        subclasses.extend(get_all_subclasses(sc))
+
+    return subclasses
