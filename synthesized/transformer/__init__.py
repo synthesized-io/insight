6af50244cb0db8e5c47097a2e7bcf0a2ab45f264
tonbadal <ton_badal@hotmail.com>
2021-03-01 14:37:20 +0000

    fix imports in tests

--- synthesized/transformer/__init__.py
+++ synthesized/transformer/__init__.py
@@ -1,11 +1,11 @@
 from .base import BagOfTransformers, SequentialTransformer, Transformer
 from .child import (BinningTransformer, CategoricalTransformer, DateCategoricalTransformer, DateToNumericTransformer,
                     DateTransformer, DropColumnTransformer, DTypeTransformer, NanTransformer, QuantileTransformer)
-from .data_frame_transformer import TransformerFactory
+from .data_frame_transformer import DataFrameTransformer, TransformerFactory
 
 __all__ = [
     'Transformer', 'BagOfTransformers', 'SequentialTransformer', 'BinningTransformer', 'CategoricalTransformer',
-    'TransformerFactory', 'DateTransformer', 'DateCategoricalTransformer',
-    'DateToNumericTransformer', 'DropColumnTransformer', 'DTypeTransformer', 'NanTransformer',
-    'QuantileTransformer'
+    'DataFrameTransformer', 'TransformerFactory', 'DateTransformer', 'DateCategoricalTransformer',
+    'DateToNumericTransformer', 'DropColumnTransformer', 'DTypeTransformer', 'NanTransformer', 'QuantileTransformer'
+
 ]
