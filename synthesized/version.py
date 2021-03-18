e15dbae9972f42818a477ec2ddb0def48ba25baa
Simon Swan <shk.swan@gmail.com>
2021-03-18 20:44:51 +0800

    ML-61 set version to 1.2.0

--- synthesized/version.py
+++ synthesized/version.py
@@ -1,6 +1,6 @@
 MAJOR: int = 1
 MINOR: int = 2
 PATCH: int = 0
-PRE_RELEASE: str = 'alpha.1'
+PRE_RELEASE: str = ''  # should start with "-" or be empty
 
-__version__ = f'{MAJOR}.{MINOR}.{PATCH}-{PRE_RELEASE}'
+__version__ = f'{MAJOR}.{MINOR}.{PATCH}{PRE_RELEASE}'
