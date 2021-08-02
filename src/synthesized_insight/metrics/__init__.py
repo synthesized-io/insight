7f60ea3b61fa494eecca24d49b296d31b3ef6172
Akanksha <akanksha@Akankshas-MBP.hyperoptic.com>
2021-08-02 10:19:44 +0100

    ML-469 Minor changes to __init__.py

--- src/synthesized_insight/metrics/__init__.py
+++ src/synthesized_insight/metrics/__init__.py
@@ -1,6 +1,6 @@
 from .metrics import (CramersV, EarthMoversDistance, KendallTauCorrelation,
                       KolmogorovSmirnovDistance, Mean, SpearmanRhoCorrelation, StandardDeviation)
-from .base import (TwoColumnMetric)
+from .base import TwoColumnMetric
 
 __all__ = ['TwoColumnMetric', 'CramersV', 'EarthMoversDistance', 'KendallTauCorrelation',
            'KolmogorovSmirnovDistance', 'Mean', 'SpearmanRhoCorrelation', 'StandardDeviation']
