d6bf7e41885a333efb6353082c0410bdf525e494
Akanksha <akanksha@Akankshas-MBP.hyperoptic.com>
2021-08-03 16:51:25 +0100

    ML-475 more tests added

--- tests/test_modelling_preprocessor.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-from src.synthesized_insight.modelling import ModellingPreprocessor
-
-
-def test_modelling_preprocessor():
-    data = pd.read_csv('datasets/credit_with_categoricals.csv')
-    p = ModellingPreprocessor(target='SeriousDlqin2yrs')
-    p.fit_transform(data)
