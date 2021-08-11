ec8e3f5f509c1bbb9ffe9e8f227f3357c9a7b771
Akanksha <33543559+akanksha1304@users.noreply.github.com>
2021-08-11 10:08:29 +0100

    ML-475: Port modelling to insight repo (#6)
    * ML-475 Porting the modelling metrics and modelling classes/methods to insight repo. Also, ported the existing tests related to these functionalities.
    
    * MML-475 Trying to fix lint issues
    
    * ML-475 Trying to fix sonar issue
    
    * ML-475 ongoing changes
    
    * ML-475 fixing a bug in preprosessor. Removing unnecessary print statements
    
    * ML-475 Fixing lint/sonar issues
    
    * ML-475 trying to fix sonar issues
    
    * ML-475 Adding more tests
    
    * ML-475 more tests added
    
    * ML-475 more tests
    
    * ML-475 more tests
    
    * ML-475 Adding more tests
    
    * ML-475 cleaning up the code
    
    * ML-475 Cleaned up the code
    
    * ML-475 minor modification
    
    * ML-475 Passing df_test arg in init rather than in call method of the PredictiveModellingScore
    
    Co-authored-by: Akanksha <akanksha@Akankshas-MBP.hyperoptic.com>

--- src/synthesized_insight/metrics/__init__.py
+++ src/synthesized_insight/metrics/__init__.py
@@ -1,6 +1,13 @@
+from .base import TwoColumnMetric
 from .metrics import (CramersV, EarthMoversDistance, KendallTauCorrelation,
                       KolmogorovSmirnovDistance, Mean, SpearmanRhoCorrelation, StandardDeviation)
-from .base import TwoColumnMetric
+from .modelling_metrics import (ROCAUC, Accuracy, ConfusionMatrix, F1Score,
+                                MeanAbsoluteError, MeanSquaredError, PRCurve,
+                                Precision, R2Score, Recall, ROCCurve,
+                                PredictiveModellingScore)
 
 __all__ = ['TwoColumnMetric', 'CramersV', 'EarthMoversDistance', 'KendallTauCorrelation',
-           'KolmogorovSmirnovDistance', 'Mean', 'SpearmanRhoCorrelation', 'StandardDeviation']
+           'KolmogorovSmirnovDistance', 'Mean', 'SpearmanRhoCorrelation', 'StandardDeviation',
+           'ROCAUC', 'Accuracy', 'ConfusionMatrix', 'F1Score', 'MeanAbsoluteError',
+           'MeanSquaredError', 'PRCurve', 'Precision', 'R2Score', 'Recall', 'ROCCurve',
+           'PredictiveModellingScore']
