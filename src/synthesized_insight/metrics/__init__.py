601318142b3337bfb655e768219ce81919673b64
Akanksha <akanksha@Akankshas-MBP.hyperoptic.com>
2021-08-13 12:07:46 +0100

    Ml-446 Fixing package source

--- src/synthesized_insight/metrics/__init__.py
+++ src/synthesized_insight/metrics/__init__.py
@@ -1,9 +1,9 @@
 from .metrics import (MetricStatistics, EarthMoversDistance, HellingerDistance, JensenShannonDivergence,
                       KolmogorovSmirnovDistance, Norm, KruskalWallis, EarthMoversDistanceBinned,
                       KullbackLeiblerDivergence, BinomialDistance, CramersV, KendallTauCorrelation,
-                      SpearmanRhoCorrelation, R2Mcfadden, DistanceNNCorrelation, DistanceCNCorrelation)
+                      SpearmanRhoCorrelation, R2Mcfadden, DistanceNNCorrelation, DistanceCNCorrelation,
+                      Mean, StandardDeviation)
 from .base import TwoColumnMetric
-from .metrics import Mean, StandardDeviation
 
 __all__ = ['TwoColumnMetric', 'CramersV', 'EarthMoversDistance', 'KendallTauCorrelation',
            'KolmogorovSmirnovDistance', 'Mean', 'SpearmanRhoCorrelation', 'StandardDeviation',
--- tests/test_modelling_metrics.py
+++ tests/test_modelling_metrics.py
@@ -2,11 +2,11 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import RidgeClassifier, Ridge, LogisticRegression
 
-from src.synthesized_insight.metrics import (PredictiveModellingScore, Accuracy,
-                                             ROCAUC, ConfusionMatrix, F1Score,
-                                             MeanAbsoluteError, MeanSquaredError, PRCurve,
-                                             Precision, R2Score, Recall, ROCCurve)
-from src.synthesized_insight.metrics.modelling_metrics import (classifier_scores,
+from src.synthesized_insight.metrics.modelling_metrics import (Accuracy, ConfusionMatrix, F1Score,
+                                                               MeanAbsoluteError, MeanSquaredError, PRCurve,
+                                                               Precision, R2Score, Recall, ROCCurve,
+                                                               PredictiveModellingScore,
+                                                               classifier_scores,
                                                                predictive_modelling_score,
                                                                classifier_scores_from_df,
                                                                regressor_scores_from_df)
