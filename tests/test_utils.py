264b035f9e8702ed437d5734c377e8dcf03c9084
Akanksha <akanksha@Akankshas-MBP.hyperoptic.com>
2021-08-18 09:09:07 +0100

    ML-446 Removing a flaky test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pandas as pd
 
-from src.synthesized_insight.metrics.utils import (bootstrap_statistic, bootstrap_binned_statistic,
-                                                   permutation_test)
+from src.synthesized_insight.metrics.utils import (bootstrap_statistic, permutation_test)
 
 
 def test_bootstrap():
@@ -10,13 +9,6 @@ def test_bootstrap():
         return np.sum((h_x - h_y)**2)
     assert bootstrap_statistic((pd.Series([1]), pd.Series([0])), distance, n_samples=100).min() == 1
     assert bootstrap_statistic((pd.Series(range(2)), pd.Series(range(2, 4))), distance, 100).max() == 8
-
-
-def test_bootstrap_binned():
-    def distance(h_x, h_y):
-        return np.linalg.norm(h_x - h_y, ord=1)
-
-    assert bootstrap_binned_statistic((pd.Series([1, 3, 0]), pd.Series([1, 4, 3])), distance).max() == 12
 
 
 def test_permutation():
