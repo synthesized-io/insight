b35fa4d35b74a4886165dcc83a047bbbefa2daa0
Simon Swan <simon@synthesized.io>
2021-08-27 17:35:48 +0100

    Add CI action workflows for testing and building (#21)
    * add ci action workflows
    
    * Update pytest coverage argument
    
    * Add sonarcloud scanning
    
    * Update license and classifiers
    
    * Use pre-commit.ci instead of action
    
    * Fix lint issues

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # synthesized-insight
+
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/synthesized-io/insight/master.svg)](https://results.pre-commit.ci/latest/github/synthesized-io/insight/master)
--- src/synthesized_insight/modelling/preprocessor.py
+++ src/synthesized_insight/modelling/preprocessor.py
@@ -38,7 +38,7 @@ class ModellingPreprocessor:
         for cat_col in self._categorical(df):
             logger.debug(f"Preprocessing value '{cat_col}'...")
             column = df[cat_col]
-            column = column.fillna('nan').astype(str)
+            column = cast(pd.Series, column.fillna('nan').astype(str))
             if self.target and cat_col == self.target:
                 x_i = column.to_numpy().reshape(-1, 1)
                 encoder = LabelEncoder()
@@ -86,7 +86,7 @@ class ModellingPreprocessor:
 
         for cat_col in self._categorical(df):
             column = df[cat_col]
-            column = column.fillna('nan').astype(str)
+            column = cast(pd.Series, column.fillna('nan').astype(str))
             encoder = self.column_encoders[cat_col]
             if self.target and cat_col == self.target:
                 x_i = encoder.transform(column.values.reshape(-1, 1)).reshape(-1, 1)
