390ddea3faebec86a3b3eb5d3ca3238f40adc609
Akanksha <akanksha@Akankshas-MacBook-Pro.local>
2021-09-01 13:58:06 +0100

    ML-446 removing dependency changes and binary test

--- tests/test_check.py
+++ tests/test_check.py
@@ -22,7 +22,7 @@ def df():
     return df
 
 
-def verify_columns_types(df, categorical_cols, continuous_cols, affine_cols, ordinal_cols, binary_cols):
+def verify_columns_types(df, categorical_cols, continuous_cols, affine_cols, ordinal_cols):
     check = ColumnCheck()
 
     pred_categorical_cols = set()
@@ -49,28 +49,21 @@ def verify_columns_types(df, categorical_cols, continuous_cols, affine_cols, ord
             pred_ordinal_cols.add(col)
     assert pred_ordinal_cols == ordinal_cols
 
-    pred_binary_cols = set()
-    for col in df.columns:
-        if check.binary(df[col]):
-            pred_binary_cols.add(col)
-    assert pred_binary_cols == binary_cols
-
 
 def test_column_check(df):
     categorical_cols = set(['string_col', 'bool_col', 'int_bool_col', 'ordered_cat_col', 'date_col'])
     continuous_cols = set(['int_col', 'float_col'])
     affine_cols = set(['date_col', 'int_col', 'float_col'])
     ordinal_cols = set(['ordered_cat_col', 'date_col', 'float_col', 'int_col', 'int_bool_col', 'bool_col'])
-    binary_cols = set(['bool_col', 'int_bool_col'])
 
-    verify_columns_types(df.copy(), categorical_cols, continuous_cols, affine_cols, ordinal_cols, binary_cols)
+    verify_columns_types(df.copy(), categorical_cols, continuous_cols, affine_cols, ordinal_cols)
 
     # Adding some NaNs to the dataframe
     df_nan = copy.deepcopy(df)
     for col in df_nan.columns:
         df_nan.loc[df_nan.sample(frac=0.1).index, col] = np.nan
 
-    verify_columns_types(df_nan, categorical_cols, continuous_cols, affine_cols, ordinal_cols, binary_cols)
+    verify_columns_types(df_nan, categorical_cols, continuous_cols, affine_cols, ordinal_cols)
 
 
 def test_same_domain_columns(df):
