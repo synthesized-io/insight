44d5335cad31368b570236874387991e3f4841d5
Simon Swan <shk.swan@gmail.com>
2021-09-14 01:01:46 +0100

    Don't import from src.synthesized_insight in tests

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from src.synthesized_insight.metrics import (
+from synthesized_insight.metrics import (
     CramersV,
     DistanceCNCorrelation,
     DistanceNNCorrelation,
