34018c6eef2da374c7b35faf4a1e4157f3b0837c
Tom Carter <tomcarter017@gmail.com>
2024-02-07 15:03:10 +0000

    Drop NaN in KolmogorovSmirnov (#175)
    * Omit NaN policy in KolmogorovSmirnov
    
    * dropna instead of nan_policy

--- src/insight/metrics/metrics.py
+++ src/insight/metrics/metrics.py
@@ -534,4 +534,5 @@ class KolmogorovSmirnovDistance(TwoColumnMetric):
         """
         if sr_a.empty or sr_b.empty:
             return 1.0
-        return ks_2samp(sr_a, sr_b)[0]  # The first element is the KS statistic
+
+        return ks_2samp(sr_a.dropna(), sr_b.dropna())[0]  # The first element is the KS statistic
